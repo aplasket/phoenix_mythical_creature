b8471157a2af5b938d1ea6b8861ee93d02d92637
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-31 22:25:49 -0400

    WIP

--- lib/phoenix.rb
+++ lib/phoenix.rb
@@ -1,1 +1,34 @@
 # Create the Phoenix class here
+class Phoenix
+  attr_reader :name,
+              :color,
+              :mood,
+              :pharaoh,
+              :emotional_awareness
+
+  def initialize(name, color = "golden", mood = "stoic", pharaoh = nil)
+    @name = name
+    @color = color
+    @mood = mood
+    @pharaoh = pharaoh
+    @emotional_awareness = Hash.new(0)
+  end
+
+  def feels_emotion(emotion)
+    @emotional_awareness[emotion]+=1
+    if @emotional_awareness[emotion] == 1
+      @color = "amber"
+      @mood = "heated"
+    elsif @emotional_awareness[emotion] == 2
+      @color = "scarlet"
+      @mood = "fiery"
+    elsif @emotional_awareness[emotion] == 3
+      @color = "crimson"
+      @mood = "ablaze"
+    elsif @emotional_awareness[emotion] == 4
+      @color = "deep violet"
+      @mood = "incandescent"
+    end
+  end
+
+end
